72359a2005e69bdd180176c1b8fd377eb784cc41
Sandeep K Nair <sandeepsuvit@gmail.com>
2019-03-16 16:45:58 +0530

    updated the comments

--- train_nlu.py
+++ train_nlu.py
@@ -8,9 +8,16 @@ from rasa_nlu.training_data import load_data
 # Function
 # -----------
 def train(data: object, config_file: object, model_dir: object) -> object:
+    # Load the training data
     training_data = load_data(data)
+
+    # Configure the trainer
     trainer = Trainer(config.load(config_file))
+
+    # Start training
     trainer.train(training_data)
+
+    # Persist the training model
     model_directory = trainer.persist(model_dir, fixed_model_name='chat')
 
 
