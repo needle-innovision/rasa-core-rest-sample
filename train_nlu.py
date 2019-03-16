11c092962488d42f2b1df9bfeabc2da4af8c7816
Sandeep K Nair <sandeepsuvit@gmail.com>
2019-03-16 16:51:15 +0530

    added unit testing

--- train_nlu.py
+++ train_nlu.py
@@ -1,13 +1,13 @@
 # Imports
 # -----------
 from rasa_nlu import config
-from rasa_nlu.model import Trainer
+from rasa_nlu.model import Trainer, Interpreter
 from rasa_nlu.training_data import load_data
 
 
 # Function
 # -----------
-def train(data: object, config_file: object, model_dir: object) -> object:
+def train_nlu(data: object, config_file: object, model_dir: object) -> object:
     # Load the training data
     training_data = load_data(data)
 
@@ -21,6 +21,18 @@ def train(data: object, config_file: object, model_dir: object) -> object:
     model_directory = trainer.persist(model_dir, fixed_model_name='chat')
 
 
+# Testing
+# -----------
+def run_nlu():
+    interpreter = Interpreter.load('./models/nlu/default/chat')
+    print(interpreter.parse(u"how many days in january?"))
+
+
 # Training
-# ------------
-train('data/nlu.md', 'nlu_config.yml', 'models/nlu')
+# -----------
+if __name__ == '__main__':
+    # Training the model
+    train_nlu('data/nlu.md', 'nlu_config.yml', 'models/nlu')
+
+    # Test the trained data
+    run_nlu()
